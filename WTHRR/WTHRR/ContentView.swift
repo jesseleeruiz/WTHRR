d1eaf31c24a78146098fa204766277f38f6d3d2b
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-06 13:02:04 -1000

    Added Xcode project.

---
+++ WTHRR/WTHRR/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WTHRR
+//
+//  Created by Jesse Ruiz on 11/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WTHRR/WTHRR/WTHRRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTHRRApp.swift
+//  WTHRR
+//
+//  Created by Jesse Ruiz on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct WTHRRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
