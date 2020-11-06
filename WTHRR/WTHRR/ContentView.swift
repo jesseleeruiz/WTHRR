30478380a93291f6aadd1a05857ebc324178e770
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-06 13:05:53 -1000

    Added color gradient to whole view.

--- WTHRR/WTHRR/ContentView.swift
+++ WTHRR/WTHRR/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
