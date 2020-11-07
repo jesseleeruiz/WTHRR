078950df214c1009cae8d204ba699d49c3e6e3d0
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-06 14:01:41 -1000

    Added HStack of weather forecast for the rest of the week. Added a new color set (lightBlue) to have a better gradient. Extracted out the individual day forecasts in the HStack to it's own struct to reuse it for the rest of the days.

--- WTHRR/WTHRR/ContentView.swift
+++ WTHRR/WTHRR/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
@@ -31,6 +31,29 @@ struct ContentView: View {
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
                 }
+                .padding(.bottom, 40)
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temperature: 74)
+                    
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "sun.max.fill",
+                                   temperature: 85)
+                    
+                    WeatherDayView(dayOfWeek: "THUR",
+                                   imageName: "wind",
+                                   temperature: 68)
+                    
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "cloud.drizzle.fill",
+                                   temperature: 66)
+                    
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "cloud.sun.rain.fill",
+                                   temperature: 70)
+                }
                 Spacer()
             }
         }
@@ -40,5 +63,29 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundColor(.white)
+        }
     }
 }
